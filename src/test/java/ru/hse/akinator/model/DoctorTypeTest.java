cd4e8153e5679c45e4bf604d43527e0e4e2acb68
kibrq <ki.br178@gmail.com>
2021-11-03 18:16:17 +0300

    Iteration 2. Tests

--- src/test/java/ru/hse/akinator/model/DoctorTypeTest.java
+++ src/test/java/ru/hse/akinator/model/DoctorTypeTest.java
@@ -33,4 +33,47 @@ public class DoctorTypeTest {
         Assertions.assertThat(doctorType.getName()).isEqualTo(name);
         Assertions.assertThat(doctorType.getDiseases()).isEqualTo(diseasesSet);
     }
+
+    public static Object[][] testSettersAndGetters_Source() {
+        return new Object[][] {
+                {0L, "", Set.of(), null, null},
+                {0L, "", Set.of(), "", Set.of()},
+                {0L, "a", Set.of(), "", Set.of()},
+                {0L, "", Set.of(), "a", Set.of()},
+                {0L, "", Set.of(), "a", TestUtils.randomDiseasesAsSet(10, 1)},
+                {0L, "a", TestUtils.randomDiseasesAsSet(10, 3), "b", TestUtils.randomDiseasesAsSet(10, 3)}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("testSettersAndGetters_Source")
+    public void testSettersAnsGetters(Long id, String name, Set<Disease> diseasesSet, String name1, Set<Disease> diseasesSet1) {
+        if (id == null || name == null || diseasesSet == null) {
+            Assertions.assertThatThrownBy(() -> DoctorType.create(id, name, diseasesSet)).isInstanceOf(IllegalArgumentException.class);
+            return;
+        }
+        DoctorType doctorType = DoctorType.create(id, name, diseasesSet);
+
+        Assertions.assertThat(doctorType.getId()).isEqualTo(id);
+        Assertions.assertThat(doctorType.getName()).isEqualTo(name);
+        Assertions.assertThat(doctorType.getDiseases()).isEqualTo(diseasesSet);
+
+        if (name1 == null) {
+            Assertions.assertThatThrownBy(() -> doctorType.setName(name1)).isInstanceOf(IllegalArgumentException.class);
+            Assertions.assertThat(doctorType.getName()).isEqualTo(name);
+        } else {
+            Assertions.assertThatCode(() -> doctorType.setName(name1)).doesNotThrowAnyException();
+            Assertions.assertThat(doctorType.getName()).isEqualTo(name1);
+        }
+
+        if (diseasesSet1 == null) {
+            Assertions.assertThatThrownBy(() -> doctorType.setDiseases(diseasesSet1)).isInstanceOf(IllegalArgumentException.class);
+            Assertions.assertThat(doctorType.getDiseases()).isEqualTo(diseasesSet);
+        } else {
+            Assertions.assertThatCode(() -> doctorType.setDiseases(diseasesSet1)).doesNotThrowAnyException();
+            Assertions.assertThat(doctorType.getDiseases()).isEqualTo(diseasesSet1);
+        }
+
+        Assertions.assertThat(doctorType.getId()).isEqualTo(id);
+    }
 }
