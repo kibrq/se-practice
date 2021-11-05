9677a6e03bbb9afcb2caacae14ee1891c82654be
kibrq <ki.br178@gmail.com>
2021-11-05 22:36:45 +0300

    Redo interface Interaction

--- src/main/java/ru/hse/akinator/interaction/QuestionFormatter.java
+++
@@ -1,9 +0,0 @@
-package ru.hse.akinator.interaction;
-
-import ru.hse.akinator.model.Symptom;
-
-public interface QuestionFormatter {
-    static String questionAboutSymptom(Symptom symptom) {
-        throw new UnsupportedOperationException();
-    }
-}
