2d24e7e813595b05e4e757ff3b4898c6b1195839
kibrq <ki.br178@gmail.com>
2021-11-05 23:14:24 +0300

    Iteration 8. Impl

--- src/main/java/ru/hse/akinator/interaction/Interaction.java
+++ src/main/java/ru/hse/akinator/interaction/Interaction.java
@@ -3,13 +3,14 @@ package ru.hse.akinator.interaction;
 import ru.hse.akinator.model.Answer;
 import ru.hse.akinator.model.Symptom;
 
+import java.io.Closeable;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
-public interface Interaction {
+public interface Interaction extends Closeable {
 
     static Interaction create(InputStream is, PrintStream outputStream, Function<Symptom, String> questionFormatter) {
         throw new UnsupportedOperationException();
