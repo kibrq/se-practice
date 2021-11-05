d2e8d7814de05ac8efb0bfa72ffaf73dc0fd5614
Ruslan Salkaev <rsalkaev@gmail.com>
2021-11-05 22:05:30 +0300

    Iter. 7 disease setters tests

--- src/test/java/ru/hse/akinator/model/DiseaseTest.java
+++ src/test/java/ru/hse/akinator/model/DiseaseTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class DiseaseTest {
 
     public static Object[][] testGetters_Source() {
-        return new Object[][] {
+        return new Object[][]{
                 {null, null, null},
                 {null, "", List.of()},
                 {0L, null, List.of()},
@@ -19,6 +19,17 @@ public class DiseaseTest {
                 {1L, "", TestUtils.symptomsFromRandomNames(5)}
         };
     }
+
+    public static Object[][] testSetters_Source() {
+        return new Object[][]{
+                {null, null, null, null, null},
+                {null, "", List.of(), null, null},
+                {0L, null, List.of(), null, null},
+                {0L, "Debil", null, null, null},
+                {0L, "You are Maxim", TestUtils.symptomsFromNames(List.of("0iq", "Tupovat")), null, TestUtils.symptomsFromNames(List.of("-3iq", "Sovsem ploh"))}
+        };
+    }
+
 
     @ParameterizedTest
     @MethodSource("testGetters_Source")
@@ -30,6 +41,31 @@ public class DiseaseTest {
             Assertions.assertThat(disease.getId()).isEqualTo(id);
             Assertions.assertThat(disease.getName()).isEqualTo(name);
             Assertions.assertThat(disease.getSymptoms()).isEqualTo(symptoms);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("testSetters_Source")
+    public void testSetters(Long id, String name, List<Symptom> symptoms, String newName, List<Symptom> newSymptoms) {
+        if (id == null || name == null || symptoms == null) {
+            Assertions.assertThatThrownBy(() -> Disease.create(id, name, symptoms)).isInstanceOf(IllegalArgumentException.class);
+        } else {
+            Disease disease = Disease.create(id, name, symptoms);
+            Assertions.assertThat(disease.getId()).isEqualTo(id);
+            Assertions.assertThat(disease.getName()).isEqualTo(name);
+            Assertions.assertThat(disease.getSymptoms()).isEqualTo(symptoms);
+            if (newName == null) {
+                Assertions.assertThatThrownBy(() -> disease.setName(newName)).isInstanceOf(IllegalArgumentException.class);
+            } else {
+                disease.setName(newName);
+                Assertions.assertThat(disease.getName()).isEqualTo(newName);
+            }
+            if (newSymptoms == null) {
+                Assertions.assertThatThrownBy(() -> disease.setSymptoms(newSymptoms)).isInstanceOf(IllegalArgumentException.class);
+            } else {
+                disease.setSymptoms(newSymptoms);
+                Assertions.assertThat(disease.getSymptoms()).isEqualTo(newSymptoms);
+            }
         }
     }
 }
