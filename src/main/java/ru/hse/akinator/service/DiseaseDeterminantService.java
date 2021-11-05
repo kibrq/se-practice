9b559bf5c1c88f8201f56705bd0b79d0f93f55d9
Ruslan Salkaev <rsalkaev@gmail.com>
2021-11-05 17:09:49 +0300

    Implement measureProbability

--- src/main/java/ru/hse/akinator/service/DiseaseDeterminantService.java
+++ src/main/java/ru/hse/akinator/service/DiseaseDeterminantService.java
@@ -12,7 +12,25 @@ import java.util.Map;
 public class DiseaseDeterminantService {
 
     public static double measureProbability(Disease disease, Map<Symptom, Answer> answerBySymptoms) {
-        throw new UnsupportedOperationException();
+        int counter = 0;
+        for (Map.Entry<Symptom, Answer> entry : answerBySymptoms.entrySet()) {
+            boolean isSymptomInDisease = disease.getSymptoms().contains(entry.getKey());
+            switch (entry.getValue()) {
+                case DEFINITELY_YES -> {
+                    counter += isSymptomInDisease ? 2 : -2;
+                }
+                case MORE_YES_THAN_NO -> {
+                    counter += isSymptomInDisease ? 1 : -1;
+                }
+                case MORE_NO_THAN_YES -> {
+                    counter += isSymptomInDisease ? -1 : 1;
+                }
+                case DEFINITELY_NO -> {
+                    counter += isSymptomInDisease ? -2 : 2;
+                }
+            }
+        }
+        return (1 + (double) (counter) / (answerBySymptoms.size() * 2)) / 2;
     }
 
     public static void sortByRelevance(List<Disease> diseases, Map<Symptom, Answer> answerBySymptoms) {
