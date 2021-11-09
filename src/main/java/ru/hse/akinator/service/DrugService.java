1632e9590c32234d0ff365a6bb12e7c949239182
Ruslan Salkaev <rsalkaev@gmail.com>
2021-11-09 17:30:25 +0300

    Add drugsService

---
+++ src/main/java/ru/hse/akinator/service/DrugService.java
@@ -0,0 +1,20 @@
+package ru.hse.akinator.service;
+
+import ru.hse.akinator.model.Disease;
+import ru.hse.akinator.model.Drug;
+import ru.hse.akinator.repository.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DrugService {
+	public static List<Drug> getAllDrugsForDisease(List<Drug> allDrugs, Disease disease) {
+		return allDrugs.stream()
+				.filter(drug -> drug.getDiseases().contains(disease))
+				.collect(Collectors.toList());
+	}
+
+	public static List<Drug> getAllDrugsForDiseaseFromRepo(Repository<Drug> drugsRepo, Disease disease) {
+		return getAllDrugsForDisease(drugsRepo.getAll(), disease);
+	}
+}
