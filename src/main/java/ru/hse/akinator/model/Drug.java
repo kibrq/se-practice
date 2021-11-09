cb0a6937704476a5305a3b02a84ae6146182535c
Ruslan Salkaev <rsalkaev@gmail.com>
2021-11-09 17:13:09 +0300

    Add Drug interface

---
+++ src/main/java/ru/hse/akinator/model/Drug.java
@@ -0,0 +1,17 @@
+package ru.hse.akinator.model;
+
+import java.util.List;
+
+public interface Drug {
+	static Drug create(Long id, String name, List<Disease> diseases) {
+		throw new UnsupportedOperationException();
+	}
+
+	String getName();
+
+	void setName(String name);
+
+	List<Disease> getDiseases();
+
+	void setDiseases(List<Disease> symptoms);
+}
