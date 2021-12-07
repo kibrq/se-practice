9bd6848b086e5b33a29131517da5b6efba6ae321
Ruslan Salkaev <rsalkaev@gmail.com>
2021-12-07 18:11:56 +0300

    Change test

--- src/test/java/ru/hse/akinator/model/DiseaseTest.java
+++ src/test/java/ru/hse/akinator/model/DiseaseTest.java
@@ -9,66 +9,60 @@ import java.util.List;
 
 public class DiseaseTest {
 
+    public static Object[][] testCreate_Source() {
+        return new Object[][]{
+            {0L, "You are Maxim", TestUtils.symptoms(List.of("0iq", "Tupovat"))},
+            {1L, "", TestUtils.symptoms(5)}
+        };
+    }
+
     public static Object[][] testGetters_Source() {
         return new Object[][]{
-                {null, null, null},
-                {null, "", List.of()},
-                {0L, null, List.of()},
-                {0L, "Debil", null},
-                {0L, "You are Maxim", TestUtils.symptoms(List.of("0iq", "Tupovat"))},
-                {1L, "", TestUtils.symptoms(5)}
+            {0L, "You are Maxim", TestUtils.symptoms(List.of("0iq", "Tupovat"))},
+            {1L, "", TestUtils.symptoms(5)}
         };
     }
 
     public static Object[][] testSetters_Source() {
         return new Object[][]{
-                {null, null, null, null, null},
-                {null, "", List.of(), null, null},
-                {0L, null, List.of(), null, null},
-                {0L, "Debil", null, null, null},
-                {0L, "You are Maxim", TestUtils.symptoms(List.of("0iq", "Tupovat")), null, TestUtils.symptoms(List.of("-3iq", "Sovsem ploh"))},
-                {0L, "You are Maxim", TestUtils.symptoms(List.of("0iq", "Tupovat")), "You are Maxim Suhodolski", null},
-                {0L, "You are Maxim", TestUtils.symptoms(List.of("0iq", "Tupovat")), "You are Maxim Suhodolski", TestUtils.symptoms(List.of("-3iq", "Sovsem ploh"))},
-                {1L, TestUtils.randomAlphabeticString(10), TestUtils.symptoms(5), TestUtils.randomAlphabeticString(10), TestUtils.symptoms(5)}
+            {0L, "You are Maxim", TestUtils.symptoms(List.of("0iq", "Tupovat")), "You are Maxim Suhodolski", TestUtils.symptoms(List.of("-3iq", "Sovsem ploh"))},
+            {1L, TestUtils.randomAlphabeticString(10), TestUtils.symptoms(5), TestUtils.randomAlphabeticString(10), TestUtils.symptoms(5)}
         };
     }
 
+    @ParameterizedTest
+    @MethodSource("testCreate_Source")
+    public void testCreate(Long id, String name, List<Symptom> symptoms) {
+        Assertions.assertThatThrownBy(() -> Disease.create(null, null, null)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Disease.create(null, name, symptoms)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Disease.create(id, null, symptoms)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Disease.create(id, name, null)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Disease.create(null, null, symptoms)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Disease.create(id, null, null)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Disease.create(null, name, null)).isInstanceOf(IllegalArgumentException.class);
+        Disease disease = Disease.create(id, name, symptoms);
+        Assertions.assertThat(disease).isNotNull();
+    }
 
     @ParameterizedTest
     @MethodSource("testGetters_Source")
     public void testGetters(Long id, String name, List<Symptom> symptoms) {
-        if (id == null || name == null || symptoms == null) {
-            Assertions.assertThatThrownBy(() -> Disease.create(id, name, symptoms)).isInstanceOf(IllegalArgumentException.class);
-        } else {
-            Disease disease = Disease.create(id, name, symptoms);
-            Assertions.assertThat(disease.getId()).isEqualTo(id);
-            Assertions.assertThat(disease.getName()).isEqualTo(name);
-            Assertions.assertThat(disease.getSymptoms()).isEqualTo(symptoms);
-        }
+        Disease disease = Disease.create(id, name, symptoms);
+        Assertions.assertThat(disease.getId()).isEqualTo(id);
+        Assertions.assertThat(disease.getName()).isEqualTo(name);
+        Assertions.assertThat(disease.getSymptoms()).isEqualTo(symptoms);
     }
 
     @ParameterizedTest
     @MethodSource("testSetters_Source")
-    public void testSetters(Long id, String name, List<Symptom> symptoms, String newName, List<Symptom> newSymptoms) {
-        if (id == null || name == null || symptoms == null) {
-            Assertions.assertThatThrownBy(() -> Disease.create(id, name, symptoms)).isInstanceOf(IllegalArgumentException.class);
-        } else {
-            Disease disease = Disease.create(id, name, symptoms);
-            Assertions.assertThat(disease.getId()).isEqualTo(id);
-            Assertions.assertThat(disease.getName()).isEqualTo(name);
-            Assertions.assertThat(disease.getSymptoms()).isEqualTo(symptoms);
-            if (newName == null) {
-                Assertions.assertThatThrownBy(() -> disease.setName(newName)).isInstanceOf(IllegalArgumentException.class);
-            } else {
-                disease.setName(newName);
-                Assertions.assertThat(disease.getName()).isEqualTo(newName);
-            }
-            if (newSymptoms == null) {
-                Assertions.assertThatThrownBy(() -> disease.setSymptoms(newSymptoms)).isInstanceOf(IllegalArgumentException.class);
-            } else {
-                disease.setSymptoms(newSymptoms);
-                Assertions.assertThat(disease.getSymptoms()).isEqualTo(newSymptoms);
-            }
-        }
+    public void testSettersNulls(Long id, String name, List<Symptom> symptoms, String newName, List<Symptom> newSymptoms) {
+        Disease disease = Disease.create(id, name, symptoms);
+        Assertions.assertThatThrownBy(() -> disease.setName(null)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> disease.setSymptoms(null)).isInstanceOf(IllegalArgumentException.class);
+        disease.setName(newName);
+        Assertions.assertThat(disease.getName()).isEqualTo(newName);
+        disease.setSymptoms(newSymptoms);
+        Assertions.assertThat(disease.getSymptoms()).isEqualTo(newSymptoms);
+        Assertions.assertThat(disease.getId()).isEqualTo(id);
     }
 }
